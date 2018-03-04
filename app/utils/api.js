5d37003ccfd148146d93a0c7cd42ed69d71e0e68
Tyler <tbuchhei@alumni.nd.edu>
2018-03-04 08:24:02 -0600

    Build logic and helpers to compare users

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,3 +1,59 @@
+async function getProfile(username) {
+  try {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    const body = await res.json();
+  
+    return body.data;
+  } catch (e) {
+    handleError(e);
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const res = await fetch(`https://api.github.com/users/${username}/repos?per_page=100`);
+    const body = await res.json();
+  
+    return body.data;
+  } catch (e) {
+    handleError(e);
+  }
+}
+
+function getStarCount(repos) {
+  return repos.reduce((count, repo) => count + repo.stargazers_count, 0);
+}
+
+function calculateScore(profile, repos) {
+  const followers = profile.followers;
+  const totalStars = getStarCount(repos);
+
+  return (followers * 3) * totalStars;
+}
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+async function getUserData(player) {
+  try {
+    const profile = await getProfile(player);
+    const repos = await getRepos(player);
+
+    return {
+      profile,
+      score: calculateScore(profile, repos)
+    }
+  } catch (e) {
+    handleError(e);
+  }
+}
+
+function sortPlayers(players) {
+  return players.sort((a,b) => b.score - a.score);
+}
+
 export async function fetchPopularRepos(lang) {
   const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${lang}&sort=stars&order=desc&type=Repositories`);
 
@@ -8,5 +64,14 @@ export async function fetchPopularRepos(lang) {
     return body.items;
   } catch (err) {
     console.log('Something went wrong while fetching repos from GitHub:', err);
+  }
+}
+
+export async function battle(players) {
+  try {
+    const playerData = await players.map(getUserData);
+    return sortPlayers(playerData);
+  } catch (e) {
+    handleError(e);
   }
 }
